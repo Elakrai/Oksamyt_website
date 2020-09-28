383d0846a60d29808fdd284bf7db40d12a557def
Elakrai <45211527+Elakrai@users.noreply.github.com>
2020-09-28 20:14:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	var owl=$('#slide_carousel');
+	owl.owlCarousel({
+		loop:true,
+		margin: 0,
+		autoplay: true,
+		items: 1,
+		autoplayTimeout:5000,
+		autoplaySpeed: 1000
+	});
+});
